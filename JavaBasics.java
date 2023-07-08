e870bacb75b7d10f19691fcaeb90b2f899399a53
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-07-08 17:16:36 +0700

    print reverse number

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class JavaBasics {
     public static void main(String args[]) {
-        printPattern(4);
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        // printPattern1(input);
+        // printPattern2(input);
+        printReverseNumber(input);
+
     }
 
-    public static void printPattern(int x) {
+    public static void printPattern1(int x) {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x; j++) {
                 if (j < x - i) {
@@ -13,4 +20,28 @@ public class JavaBasics {
             System.out.printf("\n");
         }
     }
+
+    public static void printPattern2(int x) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+
+                System.out.printf(" * ");
+
+            }
+            System.out.printf("\n");
+        }
+    }
+
+    public static void printReverseNumber(int x) {
+        int reverse = 0;
+        int temp = x;
+
+        while (temp > 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        System.out.println(reverse);
+    }
+
 }
