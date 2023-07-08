347f35b614a0bafcc410446029d86ff4645debd2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-07-08 13:07:21 +0700

    print star pattern

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,16 @@
 public class JavaBasics {
     public static void main(String args[]) {
-        System.out.println("Hello");
+        printPattern(4);
+    }
+
+    public static void printPattern(int x) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (j < x - i) {
+                    System.out.printf(" * ");
+                }
+            }
+            System.out.printf("\n");
+        }
     }
 }
