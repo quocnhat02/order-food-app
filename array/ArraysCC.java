776af8b7bb1af572540fad4eb61b2feb56d57101
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-23 13:39:28 +0700

    array - input and output

--- array/ArraysCC.java
+++ array/ArraysCC.java
@@ -1,15 +1,24 @@
 package array;
 
+import java.util.Scanner;
+
 /**
  * ArraysCC
  */
 public class ArraysCC {
     public static void main(String[] args) {
-        int marks[] = new int[50];
+        int marks[] = new int[100];
 
-        int numbers[] = { 1, 2, 3 };
+        Scanner sc = new Scanner(System.in);
 
-        String fruits[] = { "apple", "mango" };
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy : " + marks[0]);
+        System.out.println("chem : " + marks[1]);
+        System.out.println("math : " + marks[2]);
+
     }
 
 }
