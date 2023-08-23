ba0e06c6382047c69ec062384d815871360e58f2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-23 13:32:46 +0700

    array - creation

---
+++ array/ArraysCC.java
@@ -0,0 +1,15 @@
+package array;
+
+/**
+ * ArraysCC
+ */
+public class ArraysCC {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        int numbers[] = { 1, 2, 3 };
+
+        String fruits[] = { "apple", "mango" };
+    }
+
+}
