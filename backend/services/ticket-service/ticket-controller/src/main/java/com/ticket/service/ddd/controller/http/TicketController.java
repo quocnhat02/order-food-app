01ccc047d3e27fcd4dc6315b62fe7df619661d8b
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-06-09 22:37:10 +0700

    circuitbreaker

--- backend/services/ticket-service/ticket-controller/src/main/java/com/ticket/service/ddd/controller/http/TicketController.java
+++ backend/services/ticket-service/ticket-controller/src/main/java/com/ticket/service/ddd/controller/http/TicketController.java
@@ -1,6 +1,8 @@
 package com.ticket.service.ddd.controller.http;
 
 import com.ticket.service.ddd.application.service.event.EventApplicationService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +15,23 @@ public class TicketController {
     private EventApplicationService eventApplicationService;
 
     @GetMapping("/tickets")
+    @RateLimiter(name = "getTickets", fallbackMethod = "fallbackTicket")
+    @CircuitBreaker(name = "getTickets", fallbackMethod = "fallbackCircuitBreaker")
     public String getTickets(){
+        return eventApplicationService.getTickets();
+    }
+
+    public String fallbackTicket(Throwable throwable) {
+        return "Too many request";
+    }
+
+    public String fallbackCircuitBreaker(Throwable throwable) {
+        return "Ticket Service Error";
+    }
+
+    @GetMapping("/tickets2")
+    @RateLimiter(name = "backendB", fallbackMethod = "fallbackTicket")
+    public String getTickets2(){
         return eventApplicationService.getTickets();
     }
 
